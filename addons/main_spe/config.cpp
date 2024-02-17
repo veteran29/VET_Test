116335de02d2b8ef9a9d3b5fcf77b8396f2928bb
Filip Maciejewski <veteran29@users.noreply.github.com>
2024-02-17 05:54:08 +0100

    SPE Fix turret indices
    Config order defines turret indices which are important for scripted features

--- addons/main_spe/config.cpp
+++ addons/main_spe/config.cpp
@@ -33,19 +33,19 @@ class CfgVehicles {
     };
     class SPE_M4A1_Sherman_HullMG_base: SPE_M4A1_Sherman_base {
         class Turrets: Turrets {
+            class MainTurret: MainTurret {
+                class Turrets: Turrets {
+                    class CommanderOptics;
+                    class Loader_place;
+                    class CommanderMG;
+                };
+            };
+            class kurs_MG_turret;
             class CargoTurret_01;
             class CargoTurret_02;
             class CargoTurret_03;
             class CargoTurret_04;
             class CargoTurret_05;
-            class kurs_MG_turret;
-            class MainTurret: MainTurret {
-                class Turrets: Turrets {
-                    class CommanderMG;
-                    class CommanderOptics;
-                    class Loader_place;
-                };
-            };
         };
     };
     class SPE_M4A1_75: SPE_M4A1_Sherman_HullMG_base {};
@@ -56,27 +56,27 @@ class CfgVehicles {
 
         crew = "I_crew_F";
         class Turrets: Turrets {
-            class CargoTurret_01: CargoTurret_01 {};
-            class CargoTurret_02: CargoTurret_02 {};
-            class CargoTurret_03: CargoTurret_03 {};
-            class CargoTurret_04: CargoTurret_04 {};
-            class CargoTurret_05: CargoTurret_05 {};
-            class kurs_MG_turret: kurs_MG_turret {
-                gunnerType = "I_crew_F";
-            };
             class MainTurret: MainTurret {
                 gunnerType = "I_crew_F";
 
                 class Turrets: Turrets {
-                    class CommanderMG: CommanderMG {};
                     class CommanderOptics: CommanderOptics {
                         gunnerType = "I_officer_F";
                     };
                     class Loader_place: Loader_place {
                         gunnerType = "I_crew_F";
                     };
+                    class CommanderMG: CommanderMG {};
                 };
             };
+            class kurs_MG_turret: kurs_MG_turret {
+                gunnerType = "I_crew_F";
+            };
+            class CargoTurret_01: CargoTurret_01 {};
+            class CargoTurret_02: CargoTurret_02 {};
+            class CargoTurret_03: CargoTurret_03 {};
+            class CargoTurret_04: CargoTurret_04 {};
+            class CargoTurret_05: CargoTurret_05 {};
         };
     };
 };
